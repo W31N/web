716fad3e42370248332c78624a21748db9c55da0
Ivanov Ivan <timafolf2003@mail.ru>
2024-05-27 16:50:09 +0300

    new commit

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,15 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -20,6 +27,10 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors("AllowAll");
+
 app.MapControllers();
 
-app.Run();
+// Добавьте эту строку для указания порта 5000
+app.Run("http://localhost:5000");
+
